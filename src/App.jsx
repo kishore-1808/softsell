c246441a84cef142c1adfa57690e3a2f4b615252
kathiresh <23d116@psgitech.ac.in>
2025-05-09 23:04:54 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
-import HowItWorks from './components/howitworks';
+import HowItWorks from './components/HowItWorks';
 import Testimonials from './components/Testimonials';
 import Pricing from './components/Pricing';
 import FAQ from './components/FAQ';
