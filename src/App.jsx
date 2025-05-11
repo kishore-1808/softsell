da890da245004bbde265bc9708457af200eecf66
kathiresh <23d116@psgitech.ac.in>
2025-05-11 14:01:56 +0530

    kishore

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
+
 import Testimonials from './components/Testimonials';
 import Pricing from './components/Pricing';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
-import ContactForm from './components/contactform';
+
+import ContactForm from './components/ContactForm.jsx';
+import HowItWorks from './components/HowItWorks.jsx';
+
 
 
 
